7ccfa9b70dbbbd9896e19ffc0bde13f5442274d2
Demyx <info@demyx.sh>
2019-10-02 21:00:03 -0700

    Misc updates

--- travis.sh
+++ travis.sh
@@ -9,13 +9,9 @@ IFS=$'\n\t'
 DEMYX_ALPINE_VERSION=$(docker run -t --rm node:alpine cat /etc/os-release | grep VERSION_ID | cut -c 12- | sed -e 's/\r//g')
 DEMYX_DOCKER_COMPOSE_VERSION=$(docker run -t --rm demyx/docker-compose --version | awk -F '[ ]' '{print $3}' | cut -c -6 | sed -e 's/\r//g')
 
-# Replace the README.md
-[[ -f README.md ]] && rm README.md
-cp .readme README.md
-
-# Replace latest with actual versions
-sed -i "s/alpine-latest-informational/alpine-${DEMYX_ALPINE_VERSION}-informational/g" README.md
-sed -i "s/docker_compose-latest-informational/docker_compose-${DEMYX_DOCKER_COMPOSE_VERSION}-informational/g" README.md
+# Replace versions
+sed -i "s|alpine-.*.-informational|alpine-${DEMYX_ALPINE_VERSION}-informational|g" README.md
+sed -i "s|docker_compose-.*.-informational|docker_compose-${DEMYX_DOCKER_COMPOSE_VERSION}-informational|g" README.md
 
 # Push back to GitHub
 git config --global user.email "travis@travis-ci.org"
